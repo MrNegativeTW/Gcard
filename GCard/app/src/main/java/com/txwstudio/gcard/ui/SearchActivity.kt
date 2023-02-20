22633fe85557ddfc2e8b9ecc09dfa2cf58fedae3
TrevorWu <wowpapa3232@gmail.com>
2023-02-21 00:31:12 +0800

    feat: Add SearchActivity

---
+++ GCard/app/src/main/java/com/txwstudio/gcard/ui/SearchActivity.kt
@@ -0,0 +1,18 @@
+package com.txwstudio.gcard.ui
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.txwstudio.gcard.databinding.ActivitySearchBinding
+import com.txwstudio.gcard.viewmodel.SearchViewModel
+
+class SearchActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+    private val viewModel by viewModels<SearchViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+    }
+}
---
+++ GCard/app/src/main/java/com/txwstudio/gcard/viewmodel/SearchViewModel.kt
@@ -0,0 +1,10 @@
+package com.txwstudio.gcard.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel: ViewModel() {
+
+    init {
+
+    }
+}
