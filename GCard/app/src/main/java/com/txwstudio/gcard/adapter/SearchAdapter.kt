59f8eb84e8f84cadc43e517b249129d46c522a8f
Trevor <wowpapa3232@gmail.com>
2023-02-21 13:21:03 +0800

    feat: Add RecyclerView placeholder

---
+++ GCard/app/src/main/java/com/txwstudio/gcard/adapter/SearchAdapter.kt
@@ -0,0 +1,63 @@
+package com.txwstudio.gcard.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.txwstudio.gcard.data.SearchRepoResponse
+import com.txwstudio.gcard.databinding.ItemRepoCardBinding
+
+class SearchAdapter(private val onItemClicked: (String) -> Unit) :
+    ListAdapter<SearchRepoResponse, RepoCardViewHolder>(SearchResultDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoCardViewHolder {
+        val binding =
+            ItemRepoCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RepoCardViewHolder(binding) { absoluteAdapterPosition ->
+            onItemClicked(getItem(absoluteAdapterPosition).totalCount.toString())
+        }
+    }
+
+    override fun onBindViewHolder(holder: RepoCardViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+}
+
+
+class RepoCardViewHolder(
+    private val binding: ItemRepoCardBinding,
+    onItemClicked: (Int) -> Unit
+) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    init {
+//        binding.buttonTicketItemPlus.setOnClickListener {
+//            onItemClicked(absoluteAdapterPosition)
+//        }
+    }
+
+    fun bind(ticketType: SearchRepoResponse) {
+        TODO("Type")
+//        binding.textViewTicketItemName.text = ticketType.ticketTypeName
+//        binding.textViewTicketItemQuantity.text = ticketType.orderQuantity.toString()
+    }
+}
+
+private class SearchResultDiffCallback : DiffUtil.ItemCallback<SearchRepoResponse>() {
+    override fun areItemsTheSame(
+        oldItem: SearchRepoResponse,
+        newItem: SearchRepoResponse
+    ): Boolean {
+        TODO("Fix this")
+        return oldItem.items[0].id == newItem.items[0].id
+    }
+
+    override fun areContentsTheSame(
+        oldItem: SearchRepoResponse,
+        newItem: SearchRepoResponse
+    ): Boolean {
+        return oldItem == newItem
+    }
+}
--- GCard/app/src/main/java/com/txwstudio/gcard/ui/SearchActivity.kt
+++ GCard/app/src/main/java/com/txwstudio/gcard/ui/SearchActivity.kt
@@ -3,6 +3,7 @@ package com.txwstudio.gcard.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.txwstudio.gcard.adapter.SearchAdapter
 import com.txwstudio.gcard.databinding.ActivitySearchBinding
 import com.txwstudio.gcard.viewmodel.SearchViewModel
 
@@ -11,11 +12,22 @@ class SearchActivity: AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
     private val viewModel by viewModels<SearchViewModel>()
 
+    private lateinit var searchAdapter: SearchAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.searchBar)
+    }
+
+    private fun setupRecyclerView() {
+        searchAdapter = SearchAdapter { ticketTypeCode ->
+
+        }
+        binding.recyclerViewSearchResult.adapter = searchAdapter
+        // searchAdapter.submitList()
+        // searchAdapter.notifyDataSetChanged()
     }
 }
