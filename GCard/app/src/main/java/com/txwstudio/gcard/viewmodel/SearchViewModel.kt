e58ac4803bdea68cf130acd20e77bb178aa4a314
Trevor <wowpapa3232@gmail.com>
2023-02-24 09:13:31 +0800

    fix: Slow API response cause incorrect data display

--- GCard/app/src/main/java/com/txwstudio/gcard/viewmodel/SearchViewModel.kt
+++ GCard/app/src/main/java/com/txwstudio/gcard/viewmodel/SearchViewModel.kt
@@ -3,15 +3,15 @@ package com.txwstudio.gcard.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.txwstudio.gcard.data.SearchRepoApiModel
 import com.txwstudio.gcard.data.SearchResult
 import com.txwstudio.gcard.repository.GitHubRepository
 import com.txwstudio.gcard.utils.logI
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -23,12 +23,16 @@ class SearchViewModel(private val gitHubRepository: GitHubRepository) : ViewMode
     val uiState: LiveData<SearchResult<SearchRepoApiModel>>
         get() = _uiState
 
+    private lateinit var searchJob: Job
+
     fun submitSearchKeyword(keyword: String) {
         val trimmedKeyword = keyword.trim()
         if (trimmedKeyword.isEmpty()) return
         logI("Searching for: \"$keyword\"")
-        viewModelScope.launch(Dispatchers.IO) {
+        if (this::searchJob.isInitialized && searchJob.isActive) searchJob.cancel()
+        searchJob = viewModelScope.launch(Dispatchers.IO) {
             gitHubRepository.searchRepo(keyword)
+                .cancellable()
                 .flowOn(Dispatchers.IO)
                 .catch {
                     // TODO("Maybe throw something from repository?")
